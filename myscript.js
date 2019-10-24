782c77bfa1798a6755913211309c3938f0ad40d6
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-24 16:20:49 +0200

    Inserisco nell'if la lista generale: mancano ancora i FizzBuzz però

--- myscript.js
+++ myscript.js
@@ -6,24 +6,14 @@
 // genero i numeri da 1 a 100
 
 for(i = 1; i < 100; i++){
-  console.log(i);
+  // console.log(i);
   if(i % 3 === 0){
     console.log('Fizz');
   } else if (i % 5 === 0){
     console.log('Buzz');
-  } else if (i % 15 === 0){
+  } else if (i % 3 === 0 && i % 5 === 0){
     console.log('FizzBuzz');
+  } else {
+    console.log(i);
   }
 }
-
-// for(j = 0; j < 99; j = j+3){
-//   console.log(j + 3);
-// }
-//
-// for(z = 0; z < 100; z = z+5){
-//   console.log(z + 5);
-// }
-//
-// for(h = 0; h < 90; h = h+15){
-//   console.log(h + 15);
-// }
