7aabfd37c451c5afbe0d2d29bb0428536bc48ca7
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-24 16:16:06 +0200

    Inserimento FizzBuzz (ma non li sovrascrive)

--- myscript.js
+++ myscript.js
@@ -9,9 +9,10 @@ for(i = 1; i < 100; i++){
   console.log(i);
   if(i % 3 === 0){
     console.log('Fizz');
-  }
-  if(i % 5 ===0){
+  } else if (i % 5 === 0){
     console.log('Buzz');
+  } else if (i % 15 === 0){
+    console.log('FizzBuzz');
   }
 }
 
