a637a197dd17a78c46c049c4ceb1d945e19d44be
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-24 16:22:26 +0200

    else if (i % 3 === 0 && i % 5 === 0) in questo ciclo non serve

--- myscript.js
+++ myscript.js
@@ -11,9 +11,10 @@ for(i = 1; i < 100; i++){
     console.log('Fizz');
   } else if (i % 5 === 0){
     console.log('Buzz');
-  } else if (i % 3 === 0 && i % 5 === 0){
-    console.log('FizzBuzz');
   } else {
     console.log(i);
   }
 }
+
+// } else if (i % 3 === 0 && i % 5 === 0){
+//   console.log('FizzBuzz');
