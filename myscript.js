3bf90f1d77cb8199882a6b120ee29eff07af8367
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-24 16:08:31 +0200

    lascio solo i e inserisco Fizz ai multipli di tre

--- myscript.js
+++ myscript.js
@@ -7,16 +7,19 @@
 
 for(i = 0; i < 100; i++){
   console.log(i + 1);
+  if(i % 3 === 0){
+    console.log('Fizz');
+  }
 }
 
-for(j = 0; j < 99; j = j+3){
-  console.log('Fizz');
-}
-
-for(z = 0; z < 100; z = z+5){
-  console.log('Buzz');
-}
-
-for(h = 0; h < 100; h = h+15){
-  console.log('FizzBuzz');
-}
+// for(j = 0; j < 99; j = j+3){
+//   console.log(j + 3);
+// }
+//
+// for(z = 0; z < 100; z = z+5){
+//   console.log(z + 5);
+// }
+//
+// for(h = 0; h < 90; h = h+15){
+//   console.log(h + 15);
+// }
